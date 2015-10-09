3752b905ed61f9f854856141df50c478c12ec434
Zalmoxisus <zalmoxisus@gmail.com>
2015-10-09 13:42:06 +0300

    Refactor createBadge

--- src/browser/extension/background/index.js
+++ src/browser/extension/background/index.js
@@ -1,3 +1,7 @@
+import configureStore from '../../../app/store/configureStore';
 import './contextMenus';
+import createBadge from './badge';
 import './inject';
-import './main';
+
+const store = configureStore({counter: {count: 0}}, true);
+createBadge(store);
