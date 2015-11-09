df2db9ee11f2d197c4329b2c8a6e197da1edffd4
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-09 11:15:53 +0200

    Include Redux DevTools extension in the content script

--- src/browser/extension/background/inject.js
+++ src/browser/extension/background/inject.js
@@ -15,6 +15,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     fetch('http://localhost:3000/js/inject.bundle.js').then(response => {
       return response.text();
     }).then(response => {
+
+      // Include Redux DevTools extension
+      const httpRequest = new XMLHttpRequest();
+      httpRequest.open('GET', 'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd/js/inject.bundle.js');
+      httpRequest.send();
+      httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+          chrome.tabs.executeScript(tabId, { code: httpRequest.responseText, runAt: 'document_start' });
+        }
+      };
+
       chrome.tabs.executeScript(tabId, { code: response, runAt: 'document_end' });
     });
   });
