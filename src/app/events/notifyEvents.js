7fbec7fd1dd672c4192fd03d2e22e6600dd81b36
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-11 12:18:03 +0200

    Prevent dispatching updateBadge action in the content script

--- src/app/events/notifyEvents.js
+++ src/app/events/notifyEvents.js
@@ -2,9 +2,14 @@ import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../constants/ActionTypes';
 import { REHYDRATE_COMPLETE } from 'redux-persist/constants';
 import { updateBadge } from '../actions/extension';
 
-export default [
-  {
-    catch: [ INCREMENT_COUNTER, DECREMENT_COUNTER, REHYDRATE_COMPLETE ],
-    dispatch: updateBadge
-  }
-];
+let events = [];
+if (chrome.browserAction) {
+  events = [
+    {
+      catch: [INCREMENT_COUNTER, DECREMENT_COUNTER, REHYDRATE_COMPLETE],
+      dispatch: updateBadge
+    }
+  ];
+}
+
+export default events;
