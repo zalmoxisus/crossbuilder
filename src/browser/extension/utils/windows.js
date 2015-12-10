789ce08faab690d3ae265786bf4cf9d24924bf1d
Zalmoxisus <zalmoxisus@gmail.com>
2015-12-10 14:29:42 +0200

    Listen for the windows onRemoved and remove window from the list when it is closed

--- src/browser/extension/utils/windows.js
+++ src/browser/extension/utils/windows.js
@@ -1,25 +1,36 @@
 let windows = {};
 
-function focusIfExist(type, callback) {
-  if (!windows[type]) callback();
+function focusIfExist(name, callback) {
+  if (!windows[name]) callback();
   else {
-    chrome.windows.update(windows[type], {focused: true}, () => {
+    chrome.windows.update(windows[name], {focused: true}, () => {
       if (chrome.runtime.lastError) callback();
     });
   }
 }
 
-export function popWindow(type, customOptions) {
-  focusIfExist(type, () => {
+function handleClose(id) {
+  const name = Object.keys(windows).filter(key => windows[key] === id);
+  if (name) {
+    delete windows[name];
+    chrome.windows.onRemoved.removeListener(handleClose);
+  }
+}
+
+export function popWindow(name, customOptions) {
+  focusIfExist(name, () => {
     const options = {
       type: 'popup',
       left: 0, top: 0,
       width: window.screen.availWidth, height: window.screen.availHeight,
-      url: chrome.extension.getURL(type + '.html'),
+      url: chrome.extension.getURL(name + '.html'),
       ...customOptions
     };
     chrome.windows.create(options, (win) => {
-      windows[type] = win.id;
+      windows[name] = win.id;
+      if (!chrome.windows.onRemoved.hasListeners()) {
+        chrome.windows.onRemoved.addListener(handleClose);
+      }
     });
   });
 }
