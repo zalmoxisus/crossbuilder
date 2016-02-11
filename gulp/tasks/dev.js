abb3e40b2419d20413da5fe842c149adec2039d6
Zalmoxisus <zalmoxisus@gmail.com>
2016-02-11 12:04:44 +0200

    Use `webpack-hot-middleware` in order to see building in the console

--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -2,14 +2,16 @@ import gulp from 'gulp';
 import gutil from 'gulp-util';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import devConfig from '../../webpack/dev.config';
 const port = 3000;
 
 gulp.task('webpack-dev-server', () => {
   let myConfig = Object.create(devConfig);
-  new WebpackDevServer(webpack(myConfig, (err, stats) => {
+  const compiler = webpack(myConfig, (err, stats) => {
     if (err) throw new gutil.PluginError('webpack', err);
-  }), {
+  });
+  new WebpackDevServer(compiler, {
     contentBase: './dev',
     publicPath: myConfig.output.publicPath,
     stats: { colors: true },
@@ -20,4 +22,5 @@ gulp.task('webpack-dev-server', () => {
     if (err) throw new gutil.PluginError('webpack-dev-server', err);
     gutil.log('[webpack-dev-server]', `listening at port ${port}`);
   });
+  webpackHotMiddleware(compiler);
 });
