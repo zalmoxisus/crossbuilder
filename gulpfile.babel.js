1326681dcd2a592c020556422bffde0fdf39551b
Ivan Akulov <iamakulov@users.noreply.github.com>
2016-02-13 22:31:02 +0300

    Fix the “Line too long” warning

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,9 @@ import gulp from 'gulp';
 import requireDir from 'require-dir';
 requireDir('./gulp/tasks');
 
-gulp.task('default', ['replace-webpack-code', 'webpack-dev-server', 'views:dev', 'copy:dev', 'views:watch', 'copy:watch']);
+gulp.task('default',
+  ['replace-webpack-code', 'webpack-dev-server', 'views:dev',
+    'copy:dev', 'views:watch', 'copy:watch']);
 gulp.task('build:web', ['webpack:build:web', 'views:build:web', 'copy:build:web']);
 gulp.task('build:cordova', ['webpack:build:cordova', 'views:build:cordova', 'copy:build:cordova']);
 gulp.task('build:electron',
