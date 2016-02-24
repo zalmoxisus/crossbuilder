0213e73ff266a040c6e148f33804df065e381625
Phuc PNT <mail@phucpnt.com>
2016-02-24 14:31:17 +0700

    Fix issue: whenever the app tab is close/reload, the old subscribers from the app is not removed from the store.
    Support new function on background for creating safe store
    + Whenever app tabs is close/reload, remove the old subsribers

--- src/browser/extension/background/index.js
+++ src/browser/extension/background/index.js
@@ -4,6 +4,33 @@ import initBadge from './badge';
 
 configureStore(store => {
   window.store = store;
+
+  let unsubscribesList = {};
+  window.makeStore = function(tabId) {
+    const storeSubscribe = store.subscribe;
+    return {...store,
+      subscribe(...args) {
+        const unsubscribe = storeSubscribe(...args);
+        unsubscribesList[tabId] = unsubscribe;
+        return unsubscribe;
+      }
+    };
+  };
+
+  chrome.tabs.onRemoved.addListener((tabId) => {
+    if (unsubscribesList[tabId]) {
+      unsubscribesList[tabId]();
+      delete unsubscribesList[tabId];
+    }
+  });
+
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    if (unsubscribesList[tabId] && changeInfo.status === 'loading') {
+      unsubscribesList[tabId]();
+      delete unsubscribesList[tabId];
+    }
+  });
+
   createMenu();
   initBadge(store.getState().counter.count);
 
