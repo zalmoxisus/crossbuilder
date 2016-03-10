70a804f167fe4c24da328c54ba5e4d311f902ee8
Zalmoxisus <zalmoxisus@gmail.com>
2016-03-10 11:43:23 +0200

    Unsubscribe store's listeners when closing the window or popup
    Related to #34.

--- src/browser/extension/background/index.js
+++ src/browser/extension/background/index.js
@@ -4,11 +4,28 @@ import initBadge from './badge';
 
 configureStore(store => {
   window.store = store;
+  // Expose the store to extension's windows
+  window.getStore = () => {
+    let unsubscribeList = [];
+    return {
+      store: {
+        ...store,
+        subscribe(...args) {
+          const unsubscribe = store.subscribe(...args);
+          unsubscribeList.push(unsubscribe);
+          return unsubscribe;
+        }
+      },
+      unsubscribe: () => {
+        unsubscribeList.forEach(unsubscriber => { unsubscriber(); });
+      }
+    };
+  };
+
   createMenu();
   initBadge(store.getState().counter.count);
 
   if (process.env.NODE_ENV !== 'production') {
     require('./inject');
   }
-
 }, true);
--- src/browser/window/index.js
+++ src/browser/window/index.js
@@ -3,8 +3,10 @@ import { render } from 'react-dom';
 import Root from 'app/containers/Root';
 
 chrome.runtime.getBackgroundPage(background => {
+  const { store, unsubscribe } = background.getStore();
   render(
-    <Root store={background.store} />,
+    <Root store={store} />,
     document.getElementById('root')
   );
+  addEventListener('unload', unsubscribe, true);
 });
